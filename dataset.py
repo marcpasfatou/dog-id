85fd0c45a6fc0b80bdca1cdbec1f103156b48bac
marcpasfatou <dinhm1@unlv.nevada.edu>
2019-03-27 00:07:38 -0700

    Added data pipeline with batch creation Added Resnet architecture in new file Rewrote siamese network using TF instead of keras modified preprocessing step so that images are organized into directories per label

--- dataset.py
+++ dataset.py
@@ -4,8 +4,9 @@ import tensorflow as tf
 import pandas as pd
 import os
 import numpy as np
-from tensorflow.python.keras.applications.resnet50 import preprocess_input
-from tensorflow.python.keras.preprocessing.image import ImageDataGenerator
+import math, random
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+
 
 execution_path = os.getcwd()
 dataset_path = os.path.join(execution_path, "train")
@@ -15,11 +16,70 @@ tf.enable_eager_execution()
 print(os.listdir())
 
 
-train_label = pd.read_csv(os.path.join(execution_path,"label.csv"))
-image_size = 224
+train_label = pd.read_csv(os.path.join(execution_path,"labels.csv"))
+classes = pd.read_csv(os.path.join(execution_path,"classes.csv"))
+dataset_size, _ = train_label.shape
+train_size = math.floor(float(dataset_size) * .8)
+train_dataset = train_label.sample(n=train_size)
+train_datagen = ImageDataGenerator(rescale=1./255)
+#val_dataset = train_label[train_label["id"] != train_dataset["id"]]
+train_dataset.reset_index(drop=True, inplace=True)
+#val_dataset.reset_index(drop=True, inplace=True)
+#print(train_dataset)
 
-def get_batch(files,label_file, batch_size = 32):
-    """
-        Create batch of n pairs, half same class, half different class
-    """
-    
+def get_batch(batch_size=32):
+
+    left_images = []
+    right_images = []
+    target = []
+    #we choose random classes
+    categories = classes.sample(n=batch_size)
+
+    #for the second pair we choose the same category for the first half, different for the other half
+    part1 = categories[: batch_size // 2]
+    part2 = classes.sample(n=batch_size // 2)
+    categories_2 = pd.concat([part1, part2])
+    categories.reset_index(drop=True, inplace=True)
+    categories_2.reset_index(drop=True, inplace=True)
+    categories = pd.concat([categories, categories_2], axis=1)
+
+
+
+    #the label is created and store in target 1 when the pair is similar 0 when the pair is different
+    target.extend(np.ones((batch_size //2), dtype=int))
+    target.extend(np.zeros((batch_size //2), dtype=int))
+    print(target)
+
+    #we retrieve paths to images according to the category
+    for index, row in categories.iterrows():
+        left_images.append(os.path.join(dataset_path,row.iloc[0],train_dataset[train_dataset["breed"] == row.iloc[0]].sample(n=1)["id"].to_string(index=False)[1:] + ".jpg"))
+        right_images.append( os.path.join(dataset_path, row.iloc[1],train_dataset[train_dataset["breed"] == row.iloc[1]].sample(n=1)["id"].to_string(index=False)[1:]+ ".jpg"))
+
+    # the data is converted to a tensor
+    filenames_l = tf.constant(left_images)
+    filenames_r = tf.constant(right_images)
+    # `labels[i]` is the label for the image in `filenames[i].
+    labels = tf.constant(target)
+    batch = tf.data.Dataset.from_tensor_slices((filenames_l, filenames_r, target))
+    batch = batch.map(_parse_function)
+
+    return batch
+
+
+
+# Reads an image from a file, decodes it into a dense tensor, and resizes it
+# to a fixed shape.
+def _parse_function(filename_l, filename_r, label):
+  image_string_l = tf.read_file(filename_l)
+  image_decoded_l = tf.image.decode_jpeg(image_string_l)
+  image_resized_l = tf.image.resize_images(image_decoded_l, [28, 28])
+
+  image_string_r = tf.read_file(filename_r)
+  image_decoded_r = tf.image.decode_jpeg(image_string_r)
+  image_resized_r = tf.image.resize_images(image_decoded_r, [28, 28])
+  return image_resized_l, image_resized_r, label
+
+
+
+
+
--- preprocessing.py
+++ preprocessing.py
@@ -1,6 +1,7 @@
 from imageai.Detection import ObjectDetection
 import os, shutil
 from PIL import Image
+import pandas as pd
 """
     Preprocessing for the dog dataset
 
@@ -15,6 +16,8 @@ execution_path = os.getcwd()
 dataset_path = os.path.join(execution_path, "train")
 new_dataset_path = os.path.join(execution_path, "new_train")
 img_names = os.listdir(dataset_path)
+class_names = pd.read_csv(os.path.join(execution_path,"classes.csv"))
+train_label = pd.read_csv(os.path.join(execution_path,"labels.csv"))
 
 try:
     os.mkdir(os.path.join(execution_path, "new_train"))
@@ -22,6 +25,15 @@ except OSError:
     print ("Creation of the directory %s failed" % os.path.join(execution_path, "new_train"))
 else:
     print ("Successfully created the directory %s " % os.path.join(execution_path, "new_train"))
+
+for index, row in class_names.iterrows():
+    #print(row.to_string(header=False))
+    try:
+        os.mkdir(os.path.join(new_dataset_path, row["breed"]))
+    except OSError:
+        print ("Creation of the directory %s failed" % os.path.join(new_dataset_path, row["breed"]))
+    else:
+        print("Successfully created the directory %s " % os.path.join(new_dataset_path, row["breed"]))
 
 detector = ObjectDetection()
 detector.setModelTypeAsRetinaNet()
@@ -33,16 +45,20 @@ custom = detector.CustomObjects(dog=True)
 for name in img_names:
     img = Image.open(os.path.join(dataset_path, name))
     exists = os.path.isfile(os.path.join(new_dataset_path , name))
+    class_name = train_label[train_label["id"] == name[:-4]]
+    class_folder = class_name["breed"].to_string(index=False)[1:]
+    print(class_folder)
     if exists:
+        shutil.move(os.path.join(new_dataset_path, name), os.path.join(new_dataset_path, class_folder))
         continue
     #print(os.path.join(dataset_path , name))
     #print(os.path.join(new_dataset_path , name))
     _,detections = detector.detectCustomObjectsFromImage( custom_objects=custom, input_image=os.path.join(dataset_path , name), output_type = "array", minimum_percentage_probability=60)
     #print(detections)
     if not detections:
-        shutil.copy(os.path.join(dataset_path , name), new_dataset_path)
+        shutil.copy(os.path.join(dataset_path , name), os.path.join(new_dataset_path,class_folder))
     else:
         max_detection = max(detections, key=lambda x: x['percentage_probability'])
         crop = img.crop(max_detection["box_points"])
-        crop.save(os.path.join(new_dataset_path , name))
+        crop.save(os.path.join(new_dataset_path , class_folder, name))
 
